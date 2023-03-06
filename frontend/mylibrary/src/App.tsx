1da8afdad12dde124e694ecfeb86d2cd8a4e4c57
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-03-05 19:15:11 -0500

    wip: added stock image folders and CSS styles for website header and other elements

--- frontend/mylibrary/src/App.tsx
+++ frontend/mylibrary/src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">my library</div>;
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+    </div>
+  );
 }
 
 export default App;
