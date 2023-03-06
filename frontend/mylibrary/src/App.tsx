cdfdbb3e3632114dbe0445f4766bde1ec05b207b
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-03-06 17:16:40 -0500

    wip: add Footer component

--- frontend/mylibrary/src/App.tsx
+++ frontend/mylibrary/src/App.tsx
@@ -4,6 +4,7 @@ import { Carousel } from "./layouts/HomePage/Carousel";
 import { FeaturedBooks } from "./layouts/HomePage/FeaturedBooks";
 import { Heros } from "./layouts/HomePage/Heros";
 import { MyLibraryServices } from "./layouts/HomePage/MyLibraryServices";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Carousel />
       <Heros />
       <MyLibraryServices />
+      <Footer />
     </div>
   );
 }
--- frontend/mylibrary/src/layouts/NavbarAndFooter/Footer.tsx
+++ frontend/mylibrary/src/layouts/NavbarAndFooter/Footer.tsx
@@ -1,7 +1,42 @@
 export const Footer = () => {
   return (
-    <div>
-      <h1>footer</h1>
+    <div className="main-color">
+      <footer className="container d-flex flex-wrap justify-content-between align0item-center py-5 main-color">
+        <p className="col-md-4 mb-0 text-white">©2023 myLibrary</p>
+        <ul className="nav navbar-dark col-md-4 justify-content-end">
+          <li className="nav-item">
+            <a href="#" className="nav-link px-2 text-white">
+              Search Books
+            </a>
+          </li>
+          <li className="nav-item">
+            <p className="nav-link px-2 text-white">|</p>
+          </li>
+          <li className="nav-item">
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/elvis-espinoza/"
+              className="nav-link px-2 text-white"
+              rel="noreferrer"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li className="nav-item">
+            <p className="nav-link px-2 text-white">|</p>
+          </li>
+          <li className="nav-item">
+            <a
+              target="_blank"
+              href="https://github.com/elvisEspinozaN/myLibrary"
+              className="nav-link px-2 text-white"
+              rel="noreferrer"
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 };
