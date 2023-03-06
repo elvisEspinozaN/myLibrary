54807b280700995343c5b04b3bf9ca28f75a54ba
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-03-06 10:05:13 -0500

    wip: add Navbar component and update App.js to render it

---
+++ frontend/mylibrary/src/layouts/NavbarAndFooter/Navbar.tsx
@@ -0,0 +1,41 @@
+export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
+      <div className="container-fluid">
+        <span className="navbar-brand">myLibrary</span>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toogle Navigation"
+        >
+          <span className="navbar-toggle-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link">
+                Search
+              </a>
+            </li>
+          </ul>
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item m-1">
+              <a href="#" className="btn btn-outline-light">
+                Sign in
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
