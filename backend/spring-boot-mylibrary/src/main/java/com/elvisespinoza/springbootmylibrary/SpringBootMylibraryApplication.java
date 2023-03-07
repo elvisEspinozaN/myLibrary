b0f4e1ae9d701d6e76ccc29ef3a1f21247c627e4
Elvis Espinoza <elvis.espinoza.navarrete@outlook.com>
2023-03-07 12:33:06 -0500

    wip: set up backend

---
+++ backend/spring-boot-mylibrary/src/main/java/com/elvisespinoza/springbootmylibrary/SpringBootMylibraryApplication.java
@@ -0,0 +1,13 @@
+package com.elvisespinoza.springbootmylibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMylibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMylibraryApplication.class, args);
+	}
+
+}
---
+++ backend/spring-boot-mylibrary/src/test/java/com/elvisespinoza/springbootmylibrary/SpringBootMylibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvisespinoza.springbootmylibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMylibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
